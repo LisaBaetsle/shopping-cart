8096e7b92f0ade3ed1d105ceac40cb71a7768d34
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-10-28 11:57:51 +0100

    making a list of products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const [products] = useState([
+    {name: 'Custom Soap 1',
+     price: '€9.99', 
+     image: '',
+    },
+    {name: 'Custom Soap 2',
+     price: '€8.99', 
+     image: '',
+    }
+  ])
+
+  function addToCart(product) {
+    //console.log('addToCart is working')
+    setCart([...cart, product]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <button>Go to Cart ({cart.length})</button>
       </header>
+      <h1>Products</h1>
+      {products.map((product, index) => (
+        <div key={index}>
+        <h3>{ product.name }</h3>
+        <h4>{ product.price }</h4>
+        <img src={ product.image } alt={ product.name }/>
+        <button onClick={ addToCart }>Add to Cart</button>
+      </div>
+      ))}
+      
     </div>
+      
   );
 }
 
